2722f1ed70b465b526185bc8792b3df150d5fa56
Catalin Vunvulea <catalin.vunvulea@gmail.com>
2020-06-18 16:28:39 +0100

    Add a ListTile widget; using a CircleAvatar widget

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -32,47 +32,25 @@ class TransactionList extends StatelessWidget {
               //used for long lists (or if no of rows/columns is unknown; always has to have itemBuilder - elemtns included, and itemCount = number of rows/columns)
               itemBuilder: (ctx, index) {
                 return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 15,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Theme.of(context)
-                                .primaryColor, //access the setted ThemeColor
-                            width: 2,
-                          ),
-                        ),
-                        padding: EdgeInsets.all(10),
+                  elevation: 3,
+                  margin: EdgeInsets.symmetric(vertical: 3, horizontal: 10),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: EdgeInsets.all(6),
                         child: FittedBox(
-                          child: Text(
-                            "£ ${transactions[index].amount.toStringAsFixed(2)}", //string interpolation ${} / toStringAsFixed(2) = shows exactly 2 decimals after .
-                            style: TextStyle(
-                               // fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.purple),
-                          ),
+                          child: Text("£ ${transactions[index].amount}"),
                         ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(transactions[index].title,
-                              style: Theme.of(context).textTheme.headline6),
-                          Text(
-                            DateFormat("dd MMMM yyyy").format(transactions[
-                                    index]
-                                .date), //format of date can be changed in many ways, add ".'" after DateFormat to access other options
-                            style: TextStyle(
-                              color: Colors.grey,
-                            ),
-                          )
-                        ],
-                      ) //we convert the double to tex using toString
-                    ],
+                    ),
+                    title: Text(
+                      "${transactions[index].title}",
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMMMEd().format(transactions[index].date),
+                    ),
                   ),
                 );
               },
