764ab59ebdada6387863930000b0e61e5fa1e512
Catalin Vunvulea <catalin.vunvulea@gmail.com>
2020-06-19 12:43:57 +0100

    Use LayoutBuilder widget
    used to get the dimensions of the widget in which the elements are, to enable us calculate dynamic heights/widths

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -9,46 +9,58 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          height: 20,
-          child: FittedBox(
-            //forces his child to shrink if the value is too high
-            child: Text("£ ${spendingAmount.toStringAsFixed(0)}"),
-          ),
-        ), //toStringAsFixed(0) to avoid showing decimals
-        SizedBox(height: 4),
-        Container(
-          height: 60,
-          width: 10,
-          child: Stack(
-            //allows you to place elements on top of each other
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey, width: 1),
-                  color: Color.fromRGBO(220, 220, 220, 1),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              FractionallySizedBox(
-                heightFactor: percentageOfTotal,
-                child: Container(
+    return LayoutBuilder(builder: (context,
+            constraints) //used to calculate the size of this widget so we can give custom sizez to the elements inside
+        {
+      return Column(
+        children: <Widget>[
+          Container(
+            height: constraints.maxHeight * 0.12,
+            child: FittedBox(
+              //forces his child to shrink if the value is too high
+              child: Text("£ ${spendingAmount.toStringAsFixed(0)}"),
+            ),
+          ), //toStringAsFixed(0) to avoid showing decimals
+          SizedBox(height: constraints.maxHeight * 0.05),
+          Container(
+            height: constraints.maxHeight *
+                0.6, //constrains element is taken from LayoutBuilder
+            width: 10,
+            child: Stack(
+              //allows you to place elements on top of each other
+              children: <Widget>[
+                Container(
                   decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
+                    border: Border.all(color: Colors.grey, width: 1),
+                    color: Color.fromRGBO(220, 220, 220, 1),
                     borderRadius: BorderRadius.circular(10),
                   ),
                 ),
-              ), //height factor is between 0 and 1 (0% - 100%)
-            ],
+                FractionallySizedBox(
+                  heightFactor: percentageOfTotal,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ), //height factor is between 0 and 1 (0% - 100%)
+              ],
+            ),
           ),
-        ),
-        SizedBox(
-          height: 5,
-        ),
-        Text(label)
-      ],
-    );
+          SizedBox(
+            height: constraints.maxHeight * 0.05,
+          ),
+          Container(
+            height: constraints.maxHeight * 0.12,
+            child: FittedBox(
+              child: Text(
+                label,
+              ),
+            ),
+          )
+        ],
+      );
+    });
   }
 }
